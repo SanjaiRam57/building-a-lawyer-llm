4f498449b68724c9b01e65b6021b9d9323f6f9c1
SanjaiRam57 <sanjairam676@gmail.com>
2025-12-26 09:25:34 +0530

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Lawyer Chatbot',
+    version= '0.0.1',
+    author= 'sanjai ram',
+    author_email= 'sanjairam676@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
